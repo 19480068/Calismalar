2b01ed27f06964016a9881bd04382bcd13285026
19480068 <75071964+19480068@users.noreply.github.com>
2021-01-11 17:55:05 +0300

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# %%
+var = 15
+var1 = 14
+var2 = 13
+var3 = 12
+var4 = 11
+var5=10
+var6=9
+var7=8
+varr="Hadtanın 1. Günüdür"
+# %%
+
+gun1="pazartesi"
